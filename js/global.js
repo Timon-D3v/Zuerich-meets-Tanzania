438afc41416fb6b432a29f630a6e97958df28f6c
Timon-D3v <fiedlertimon@gmail.com>
2024-01-22 15:31:42 +0100

    # Added .gitignore # Added hamburger css # Added topbar svgs # Added global.js

---
+++ js/global.js
@@ -0,0 +1,18 @@
+new Event("open-navigation");
+
+const root = $(":root");
+
+$("#menu").on("click", () => {
+	$("#menu").toggleClass("is-active");
+	$(document).trigger("open-navigation");
+});
+
+$(document).on("open-navitagtion", () => {
+	console.log("Triggerd nav");
+});
+
+function changeTheme() {
+	root.attr("data-theme") === "dark" ?
+	root.attr("data-theme", "light") :
+	root.attr("data-theme", "dark");
+};
