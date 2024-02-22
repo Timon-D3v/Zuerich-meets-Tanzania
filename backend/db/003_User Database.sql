fb2ca764eb943e4811d8039d3de6121aa2903813
Timon-D3v <fiedlertimon@gmail.com>
2024-02-23 00:04:41 +0100

    # Updated the colors in the login # Added a background image # Added the database for users

---
+++ backend/db/003_User Database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `zmt`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(256) NULL,
+  `password` VARCHAR(256) NULL,
+  `name` VARCHAR(256) NULL,
+  `family_name` VARCHAR(256) NULL,
+  `email` VARCHAR(256) NULL,
+  `picture` LONGTEXT NULL,
+  `phone` VARCHAR(16) NULL DEFAULT '\"Keine Nummer\"',
+  `type` VARCHAR(256) NULL DEFAULT '\"user\"',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
